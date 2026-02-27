ddd1456ea1d5ad5e63fcc70e2c681d14719ecc73
Marley Reeves <marley@marlstar.com>
2026-02-27 22:53:18 +1000

    noctalia notifications

--- modules/home/desktop/hyprland/modules/notifications.nix
+++ modules/home/desktop/hyprland/modules/notifications.nix
@@ -1,11 +1,13 @@
 { ... }: {
 	wayland.windowManager.hyprland.settings = {
 		bind = [
-			"$mainMod, N, exec, swaync-client -t" # toggle
-			"$shiftMod, N, exec, swaync-client -C && swaync-client -cp" # remove latest notification and close
+			# "$mainMod, N, exec, swaync-client -t" # toggle
+			# "$shiftMod, N, exec, swaync-client -C && swaync-client -cp" # remove latest notification and close
+			"$mainMod, N, exec, noctalia-shell ipc call notifications toggleHistory"
+			"$shiftMod, N, exec, noctalia-shell ipc call notifications clear"
 
 			# reload
-			"$shiftMod, R, exec, swaync-client --reload-config & swaync-client -rs"
+			# "$shiftMod, R, exec, swaync-client --reload-config & swaync-client -rs"
 		];
 	};
 }
