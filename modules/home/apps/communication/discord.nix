71fe4910e6a77ae503c119c587a6b508aac599f0
Marley Reeves <marley@marlstar.com>
2026-02-28 10:18:14 +1000

    remove no-longer-required manual equicord version

--- modules/home/apps/communication/discord.nix
+++ modules/home/apps/communication/discord.nix
@@ -1,14 +1,4 @@
-{ pkgs, ... }: let
-equicord-upd = (pkgs.equicord.overrideAttrs (oldAttrs: {
-	version = "2026-01-25";
-	src = pkgs.fetchFromGitHub {
-		owner = "Equicord";
-		repo = "Equicord";
-		tag = "2026-02-08";
-		hash = "sha256-uk4kF6i00jAnHQ8ldjTjO4zv0S+OmMZ5zOqfRb8XWS8=";
-	};
-}));
-in {
+{ pkgs, ... }: {
 	programs.discord = {
 		enable = true;
 		settings = {
@@ -17,15 +7,10 @@ in {
 		};
 		package = (pkgs.discord.override {
 			withEquicord = true;
-			equicord = equicord-upd;
 			# withOpenASAR = true;
 			disableUpdates = false; # home-manager does this
 		});
 	};
-
-	home.packages = [
-		equicord-upd
-	];
 
 	xdg.autostart.entries = [
 		"${pkgs.discord}/share/applications/discord.desktop"
