a7fae58990a500a74ade271377b25870f930117b
Marley Reeves <marley@marlstar.com>
2026-03-05 14:30:41 +1000

    signal: fix password store tomfoolery

--- modules/home/apps/communication/signal.nix
+++ modules/home/apps/communication/signal.nix
@@ -1,5 +1,15 @@
-{ pkgs, ... }: {
+{ pkgs, ... }: let
+flags = "--password-store=gnome-libsecret";
+in {
 	home.packages = with pkgs; [
-		signal-desktop
+		(symlinkJoin {
+			name = "signal-desktop";
+			paths = [ pkgs.signal-desktop ];
+			buildInputs = [ pkgs.makeWrapper ];
+			postBuild = ''
+			wrapProgram $out/bin/signal-desktop \
+				--add-flags "${flags}"
+			'';
+		})
 	];
 }
