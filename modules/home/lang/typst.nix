88ee4d983522a25442e17ddea15ade7114afd0a2
Marley Reeves <marley@marlstar.com>
2026-03-19 09:04:41 +1000

    typst

---
+++ modules/home/lang/typst.nix
@@ -0,0 +1,18 @@
+{ pkgs, ... }: {
+	home.packages = with pkgs; [ typst ]
+	++ (with pkgs.typstPackages; [
+		wordometer
+	])
+	++ [ # LSP stuff
+		tinymist
+		websocat
+	];
+
+	# Used by typst-preview
+	programs.firefox = {
+		enable = true;
+		profiles.typst-preview = {
+			name = "typst-preview";
+		};
+	};
+}
