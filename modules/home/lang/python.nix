8ac85546cf32fc167296a9c56e2206683198ea35
Marley Reeves <marley@marlstar.com>
2026-03-26 13:05:50 +1000

    python + nix-ld

--- modules/home/lang/python.nix
+++ modules/home/lang/python.nix
@@ -1,12 +1,15 @@
-{ pkgs, ... }: {
-	home.packages = with pkgs.python314Packages; [
-		pkgs.python314
-
-		numpy
-	];
+{ osConfig, lib, ... }: {
+	# home.packages = with pkgs.python314Packages; [
+	# 	pkgs.python314
+	#
+	# 	numpy
+	# 	tkinter
+	# ];
 
 	programs.uv.enable = true;
 	programs.ty.enable = true;
 
 	programs.fish.shellAbbrs.urm = "uv run main.py";
+
+	home.sessionPath = lib.mkIf (!osConfig.environment.localBinInPath) ["$HOME/.local/bin"];
 }
