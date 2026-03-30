a45c29cddd0f0cff4f3ce93de87d39e8b624f3db
Marley Reeves <marley@marlstar.com>
2026-03-30 12:26:55 +1000

    autostart youtube music

--- modules/home/apps/entertainment/youtube-music.nix
+++ modules/home/apps/entertainment/youtube-music.nix
@@ -2,4 +2,8 @@
 	home.packages = with pkgs; [
 		pear-desktop
 	];
+
+	xdg.autostart.entries = [
+		"${pkgs.pear-desktop}/share/applications/${(builtins.elemAt pkgs.pear-desktop.desktopItems 0).name}"
+	];
 }
