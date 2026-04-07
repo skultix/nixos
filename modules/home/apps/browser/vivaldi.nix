9c05da635a0c81ff80593b1514307ecbc616bf81
Marley Reeves <marley@marlstar.com>
2026-04-07 22:03:52 +1000

    vivaldi: default browser

--- modules/home/apps/browser/vivaldi.nix
+++ modules/home/apps/browser/vivaldi.nix
@@ -4,7 +4,19 @@
 		vivaldi-ffmpeg-codecs
 	];
 
-	xdg.autostart.entries = [
-		"${pkgs.vivaldi}/share/applications/vivaldi-stable.desktop"
-	];
+	xdg = {
+		autostart.entries = [
+			"${pkgs.vivaldi}/share/applications/vivaldi-stable.desktop"
+		];
+
+		mimeApps.defaultApplications = let
+		browser = "${pkgs.vivaldi}/share/vivaldi-stable.desktop";
+		in {
+			"text/html" = browser;
+			"x-scheme-handler/http" = browser;
+			"x-scheme-handler/https" = browser;
+			"x-scheme-handler/about" = browser;
+			"x-scheme-handler/unknown" = browser;
+		};
+	};
 }
---
+++ modules/home/apps/tools/jabref.nix
@@ -0,0 +1,17 @@
+{ pkgs, ... }: let
+jabref-with-chromium-host = pkgs.jabref.overrideAttrs (old: {
+	installPhase = (old.installPhase or "") + ''
+	install -Dm644 jabgui/buildres/linux/native-messaging-host/chromium/org.jabref.jabref.json $out/etc/chromium/native-messaging-hosts/org.jabref.jabref.json
+	sed -i -e "s|/opt/jabref|$out|" $out/etc/chromium/native-messaging-hosts/org.jabref.jabref.chromium.json
+	'';
+});
+in {
+	home.packages = [
+		# jabref-with-chromium-host
+		pkgs.jabref
+	];
+
+	# programs.vivaldi.nativeMessagingHosts = [
+	# 	jabref-with-chromium-host
+	# ];
+}
