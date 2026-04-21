4ed316a32c2fc4faa72bbd81d05a3c4ab0f508cd
Marley Reeves <marley@marlstar.com>
2026-04-21 15:15:14 +1000

    fix(niri): correct lock bind

--- modules/home/desktop/niri/modules/lock.nix
+++ modules/home/desktop/niri/modules/lock.nix
@@ -2,6 +2,6 @@
 	programs.niri.settings = let
 	lock-cmd = "noctalia-shell ipc call lockScreen lock";
 	in {
-		"Mod+Control+L".action.spawn-sh = lock-cmd;
+		binds."Mod+Control+L".action.spawn-sh = lock-cmd;
 	};
 }
