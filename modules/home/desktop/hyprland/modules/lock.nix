5c1e936b164970d2d8deacec4dc8e679ed0a1808
Marley Reeves <marley@marlstar.com>
2026-04-22 09:22:01 +1000

    hyprland: use noctalia lock

--- modules/home/desktop/hyprland/modules/lock.nix
+++ modules/home/desktop/hyprland/modules/lock.nix
@@ -1,6 +1,7 @@
 { ... }: {
 	wayland.windowManager.hyprland.settings = {
-		"$lock" = "hyprlock";
+		# "$lock" = "hyprlock";
+		"$lock" = "noctalia-shell ipc call lockScreen lock";
 
 		bind = [
 			"$ctrlMod, L, exec, $lock"
