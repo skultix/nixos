0311a273b9c36cadd15dcbae60b80dc8a3da13a1
Marley Reeves <marley@marlstar.com>
2026-04-21 15:14:59 +1000

    fix(niri): use proper output mode syntax

--- modules/home/desktop/niri/modules/outputs.nix
+++ modules/home/desktop/niri/modules/outputs.nix
@@ -2,11 +2,17 @@
 	programs.niri.settings = {
 		# TODO: make monitors a system configuration.nix option
 		outputs.eDP-1 = { # Laptop screen
-			mode = "1920x1200";
+			mode = {
+				width = 1920;
+				height = 1200;
+			};
 		};
 
 		outputs.DP-6 = { # Monitor
-			mode = "3440x1440";
+			mode = {
+				width = 3440;
+				height = 1440;
+			};
 		};
 	};
 }
