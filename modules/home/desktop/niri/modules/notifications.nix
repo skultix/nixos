5d9ee8de0933c730bfd54c955a7975e2eb7dd5d3
Marley Reeves <marley@marlstar.com>
2026-05-05 22:54:39 +1000

    fix(niri): notification binds

--- modules/home/desktop/niri/modules/notifications.nix
+++ modules/home/desktop/niri/modules/notifications.nix
@@ -2,7 +2,7 @@
 	programs.niri.settings = {
 		binds = let
 		ipc = action: "noctalia-shell ipc call ${action}";
-		notifications = action: { spawn-sh = ipc action; };
+		notifications = action: { spawn-sh = ipc "notifications ${action}"; };
 		in {
 			"Mod+N".action = notifications "toggleHistory";
 			"Mod+Shift+N".action = notifications "clear";
