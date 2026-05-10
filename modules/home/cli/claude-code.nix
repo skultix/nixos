ddf292a3d625dc95e6deeeb3f607dc4d7ebd33dd
Marley Reeves <marley@marlstar.com>
2026-05-11 09:43:39 +1000

    claude-monitor

--- modules/home/cli/claude-code.nix
+++ modules/home/cli/claude-code.nix
@@ -1,7 +1,11 @@
-{ inputs, ... }: {
+{ inputs, pkgs, ... }: {
 	programs.claude-code = {
 		enable = true;
 	};
+
+	home.packages = with pkgs; [
+		claude-monitor
+	];
 
 	# claude-switch (my script)
 	programs.fish.plugins = [{
