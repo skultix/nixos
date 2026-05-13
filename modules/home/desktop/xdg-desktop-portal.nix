0618f62d80d648bb84c69ca01b9793e0933d1718
Marley Reeves <marley@marlstar.com>
2026-05-13 22:03:44 +1000

    xdg desktop portal

---
+++ modules/home/desktop/xdg-desktop-portal.nix
@@ -0,0 +1,24 @@
+{ pkgs, lib, ... }: {
+	xdg.portal = {
+		enable = true;
+		xdgOpenUsePortal = true;
+		extraPortals = with pkgs; [
+			xdg-desktop-portal-gtk
+			xdg-desktop-portal-gnome
+		];
+		config = let
+		portals = attrs: lib.mapAttrs' (key: val: lib.nameValuePair "org.freedesktop.impl.portal.${key}" val) attrs;
+		in {
+			common = {
+				default = "gnome";
+			} // portals {
+				# By far the best imo
+				FileChooser = "gnome";
+			};
+		};
+	};
+
+	home.sessionVariables = {
+		GTK_USE_PORTAL = 1;
+	};
+}
