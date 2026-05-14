2a82367300f69daa45d999910c3325b474971adb
Marley Reeves <marley@marlstar.com>
2026-05-14 11:30:52 +1000

    don't use niri-flake polkit

--- modules/nixos/desktop/niri.nix
+++ modules/nixos/desktop/niri.nix
@@ -3,4 +3,8 @@
 		enable = true;
 		package = pkgs.niri;
 	};
+
+	# niri-flake automaticlly creates a polkit daemon
+	# but I want to use polkit gnome as per home/security/polkit-agent.nix
+	systemd.user.services.niri-flake-polkit.enable = false;
 }
