89cafae24ba0d0170ad1c5aa7b12e693686d494e
Marley Reeves <marley@marlstar.com>
2026-05-14 11:02:00 +1000

    mission center: open normal size

--- modules/home/desktop/niri/modules/mission-center.nix
+++ modules/home/desktop/niri/modules/mission-center.nix
@@ -1,5 +1,13 @@
 { ... }: {
 	programs.niri.settings = {
 		binds."Mod+Shift+Escape".action.spawn-sh = "missioncenter";
+
+		window-rules = [{
+			matches = [
+				{ app-id = "io.missioncenter.MissionCenter"; }
+			];
+
+			open-fullscreen = false;
+		}];
 	};
 }
