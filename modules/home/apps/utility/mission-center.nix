17000560c0235a3d1adfb2fc2331e76c94312de5
Marley Reeves <marley@marlstar.com>
2026-05-14 11:24:05 +1000

    mission center settings

--- modules/home/apps/utility/mission-center.nix
+++ modules/home/apps/utility/mission-center.nix
@@ -2,4 +2,10 @@
 	home.packages = with pkgs; [
 		mission-center
 	];
+
+	dconf.settings."io/missioncenter/MissionCenter" = {
+		window-selected-page = "apps-page";
+		performance-sliding-graphs = true;
+		performance-smooth-graphs = true;
+	};
 }
