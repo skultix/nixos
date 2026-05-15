76ed3b8b3a98ce0822da9a901ae11f708237fcd4
Marley Reeves <marley@marlstar.com>
2026-05-15 14:20:23 +1000

    fix(niri): actually use configured resolutions

--- modules/home/desktop/niri/modules/outputs.nix
+++ modules/home/desktop/niri/modules/outputs.nix
@@ -1,5 +1,5 @@
 { osConfig, lib, ... }: let
-monitor-map = lib.mapAttrs' (monitor: cfg: lib.nameValuePair monitor { mode.width = 1920; mode.height = 1080; });
+monitor-map = lib.mapAttrs' (monitor: cfg: lib.nameValuePair monitor { mode.width = cfg.width; mode.height = cfg.height; });
 in {
 	programs.niri.settings.outputs = monitor-map osConfig.cfg.hardware.monitors;
 }
