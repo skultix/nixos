dcfc96f0f520b7629bbeb835a43b59c52047cb89
Marley Reeves <marley@marlstar.com>
2026-05-18 11:16:35 +1000

    lutris temporary fix for openldap

--- modules/home/games/lutris.nix
+++ modules/home/games/lutris.nix
@@ -3,6 +3,23 @@ gamecfg = osConfig.cfg.games;
 in lib.mkIf gamecfg.lutris.enable {
 	programs.lutris = {
 		enable = true;
+		package = lib.warn "lutris openldap temporarily overridden"
+		(pkgs.lutris.override {
+			# Intercept buildFHSEnv to modify target packages
+			buildFHSEnv = args: pkgs.buildFHSEnv (args // {
+				multiPkgs = envPkgs:
+				let
+				# Fetch original package list
+				originalPkgs = args.multiPkgs envPkgs;
+
+				# Disable tests for openldap
+				customLdap = envPkgs.openldap.overrideAttrs (_: { doCheck = false; });
+				in
+				# Replace broken openldap with the custom one
+				builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
+			});
+		});
+
 		extraPackages = with pkgs; [
 			winetricks
 			protontricks
