cf991675814141c62ed3c5b6d47cdc654f436aac
Marley Reeves <marley@marlstar.com>
2026-05-18 10:09:57 +1000

    kernel: sleep s2idle

--- modules/nixos/os/kernel.nix
+++ modules/nixos/os/kernel.nix
@@ -1,4 +1,8 @@
 { pkgs, ... }: {
 	boot.kernelPackages = pkgs.linuxPackages_zen;
 	boot.kernelModules = [ "uinput" ];
+	boot.kernelParams = [
+		# speed up waking from sleep by not doing a full s3 deep sleep
+		"mem_sleep_default=s2idle"
+	];
 }
