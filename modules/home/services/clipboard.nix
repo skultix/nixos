589b6e6919692366443986dfe47963eddae95485
Marley Reeves <marley@marlstar.com>
2026-05-18 13:43:29 +1000

    clipboard: persist and `clip` helper

--- modules/home/services/clipboard.nix
+++ modules/home/services/clipboard.nix
@@ -1,3 +1,19 @@
-{ ... }: {
-	# services.clipman.enable = true;
+{ pkgs, ... }: {
+	services.wl-clip-persist = {
+		enable = true;
+	};
+
+	home.packages = let
+	wlclip-pkg = pkgs.wl-clipboard-rs;
+	in [
+		wlclip-pkg
+
+		(pkgs.writeShellScriptBin "clip" ''
+		if [ -t 0 ]; then
+			${wlclip-pkg}/bin/wl-paste
+		else
+			${wlclip-pkg}/bin/wl-copy
+		fi
+		'')
+	];
 }
