f187bc54d950e49f355a10e1ccb2e1f4c889f4c5
Marley Reeves <marley@marlstar.com>
2026-05-22 08:37:24 +1000

    codex: don't manage config.toml
    why does codex have to use global config for *everything*

--- modules/home/cli/codex.nix
+++ modules/home/cli/codex.nix
@@ -1,12 +1,5 @@
 { ... }: {
 	programs.codex = {
 		enable = true;
-		settings = {
-			tui = {
-				status_line = ["model" "current-dir" "context-usage" "five-hour-limit" "weekly-limit"];
-				terminal_title = ["project" "git-branch" "status" "thread" "task-progress"];
-				vim_mode_default = true;
-			};
-		};
 	};
 }
