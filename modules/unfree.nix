429ec614953763e8f313151da062e86a601ef874
Marley Reeves <marley@marlstar.com>
2026-05-26 12:53:50 +1000

    unfree

--- modules/unfree.nix
+++ modules/unfree.nix
@@ -1,4 +1,7 @@
 { ... }: {
 	nixpkgs.config.allowUnfree = true;
 	environment.sessionVariables.NIXPKGS_ALLOW_UNFREE = 1;
+
+	hm.nixpkgs.config.allowUnfree = true;
+	hm.home.sessionVariables.NIXPKGS_ALLOW_UNFREE = 1;
 }
