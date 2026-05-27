f6b458dbe944861358ddda6d18302b8831e3c660
Marley Reeves <marley@marlstar.com>
2026-05-27 10:19:29 +1000

    niri media control: ytm specific

--- modules/home/desktop/niri/modules/media-control.nix
+++ modules/home/desktop/niri/modules/media-control.nix
@@ -5,12 +5,16 @@
 			allow-when-locked = true;
 			action.spawn-sh = action;
 		};
+
+		default-player = "YoutubeMusic";
+		playerctl-flags = ["-p=${default-player}"];
+		playerctl = "playerctl ${builtins.concatStringsSep " " playerctl-flags}";
 		in {
 			# Win+PageUp
-			"Mod+Prior" = locked-spawn-sh "playerctl next";
+			"Mod+Prior" = locked-spawn-sh "${playerctl} next";
 			# Win+PageDown
-			"Mod+Next" = locked-spawn-sh "playerctl previous";
-			"Pause" = locked-spawn-sh "playerctl play-pause";
+			"Mod+Next" = locked-spawn-sh "${playerctl} previous";
+			"Pause" = locked-spawn-sh "${playerctl} play-pause";
 		};
 	};
 }
