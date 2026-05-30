1f03b9d932244c062ef58f0a8614c7f6c08b2695
Marley Reeves <marley@marlstar.com>
2026-05-31 00:57:13 +1000

    trusted users + @wheel

--- modules/nixos/nix/settings.nix
+++ modules/nixos/nix/settings.nix
@@ -1,5 +1,6 @@
 { ... }: {
 	nix.settings = {
+		trusted-users = [ "root" "@wheel" ];
 		experimental-features = [ "nix-command" "flakes" ];
 		download-buffer-size = 524288000; # Fixes the "download buffer full" warning
 	};
