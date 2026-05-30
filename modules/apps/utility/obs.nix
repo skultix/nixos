f320084d91d810f9bddce0d014fb94c9078c741b
Marley Reeves <marley@marlstar.com>
2026-05-30 22:58:43 +1000

    obs

---
+++ modules/apps/utility/obs.nix
@@ -0,0 +1,16 @@
+{ pkgs, ... }: {
+	programs.obs-studio = {
+		enable = true;
+
+		package = (pkgs.obs-studio.override { cudaSupport = true; });
+
+		plugins = with pkgs.obs-studio-plugins; [
+			wlrobs
+			obs-backgroundremoval
+			obs-pipewire-audio-capture
+			obs-vaapi #optional AMD hardware acceleration
+			obs-gstreamer
+			obs-vkcapture
+		];
+	};
+}
