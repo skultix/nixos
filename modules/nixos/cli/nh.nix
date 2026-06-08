7b67fcc15b6ecddb4c346070d4f1f8341f4e7537
Marley Reeves <marley@marlstar.com>
2026-06-08 14:20:17 +1000

    nh: set hostname for flake

--- modules/nixos/cli/nh.nix
+++ modules/nixos/cli/nh.nix
@@ -1,10 +1,10 @@
-{ ... }: {
+{ config, ... }: {
 	programs.nh = {
 		enable = true;
 		clean = {
 			enable = true;
 			extraArgs = "--keep-since 4d --keep 3";
 		};
-		flake = "/home/themarlstar/nixos"; # sets NH_OS_FLAKE variable for you
+		flake = "/home/themarlstar/nixos#${config.networking.hostName}"; # sets NH_OS_FLAKE variable for you
 	};
 }
