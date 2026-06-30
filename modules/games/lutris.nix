d1d168f5f349efdee063d857508c85dff15e941a
Marley Reeves <marley@marlstar.com>
2026-06-30 23:14:15 +1000

    lutris: get rid of override now things are fixed

--- modules/games/lutris.nix
+++ modules/games/lutris.nix
@@ -3,22 +3,6 @@ gamecfg = config.cfg.games;
 in lib.mkIf gamecfg.lutris.enable (home {
 	programs.lutris = {
 		enable = true;
-		package = lib.warn "lutris openldap temporarily overridden"
-		(pkgs.lutris.override {
-			# Intercept buildFHSEnv to modify target packages
-			buildFHSEnv = args: pkgs.buildFHSEnv (args // {
-				multiPkgs = envPkgs:
-				let
-				# Fetch original package list
-				originalPkgs = args.multiPkgs envPkgs;
-
-				# Disable tests for openldap
-				customLdap = envPkgs.openldap.overrideAttrs (_: { doCheck = false; });
-				in
-				# Replace broken openldap with the custom one
-				builtins.filter (p: (p.pname or "") != "openldap") originalPkgs ++ [ customLdap ];
-			});
-		});
 
 		extraPackages = with pkgs; [
 			winetricks
