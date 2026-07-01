e44b4edf2bc7ee82b5070dfeb43ed89928924b5d
Marley Reeves <marley@marlstar.com>
2026-07-01 11:34:43 +1000

    lazygit: override GPG

--- modules/cli/git/lazygit.nix
+++ modules/cli/git/lazygit.nix
@@ -1,10 +1,13 @@
-{ home, ... }: home {
+{ home, config, ... }: home {
 	programs.lazygit = {
 		enable = true;
 		settings = {
 			quitOnTopLevelReturn = true;
 			gui = {
 				nerdFontsVersion = "3";
+			};
+			git = {
+				overrideGpg = config.hm.services.gpg-agent.enable;
 			};
 		};
 	};
