07af2e423ee2e15896aa9d55390ed74cc51b3d48
Marley Reeves <marley@marlstar.com>
2026-07-03 10:19:38 +1000

    fix(niri): make polkit daemon disable system-level

--- modules/desktop/niri/niri.nix
+++ modules/desktop/niri/niri.nix
@@ -3,11 +3,12 @@
 		enable = true;
 		package = pkgs.niri;
 	};
-}
-// home {
+
 	# niri-flake automaticlly creates a polkit daemon
 	# but I want to use my own configured polkit agent
 	systemd.user.services.niri-flake-polkit.enable = false;
+}
+// home {
 
 	home.packages = with pkgs; [
 		xwayland-satellite
