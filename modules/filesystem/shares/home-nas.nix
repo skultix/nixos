bf1e32f74e26b7bc7ff8d85933cd941c978cc3b9
Marley Reeves <marley@marlstar.com>
2026-07-03 10:35:55 +1000

    fix(nas): use lib.mkMerge for merging

--- modules/filesystem/shares/home-nas.nix
+++ modules/filesystem/shares/home-nas.nix
@@ -18,42 +18,41 @@ in {
 
 	config = let
 	cfg = config.cfg.filesystems.home-nas;
-	base = cfg.mounts-base;
+	in lib.mkMerge [
+		{ age.secrets."nas/creds".file = ../../../../secrets/nas/creds.age; }
 
-	in {
-		age.secrets."nas/creds".file = ../../../../secrets/nas/creds.age;
-	}
-	// lib.mkIf cfg.enable {
-		fileSystems = let
-		creds = config.age.secrets."nas/creds".path;
-		mkNasShare = share: {
-			device = address share;
-			fsType = "cifs";
-			options = [
-				"credentials=${creds}"
-				# Auto mount
-				"x-systemd.automount"
-				# unmount after 60s inactive
-				"x-systemd.idle-timeout=${toString unmount-after-idle-secs}"
-				# mount timeouts
-				"x-systemd.mount-timeout=${toString timeout}s"
-				"x-systemd.device-timeout=${toString timeout}s"
-				"_netdev" # mark as network dependent
-				"noauto" # don't mount at boot
-				"nofail" # don't fail hard
-				# so I own the files and dirs
-				"uid=${toString config.users.users.themarlstar.uid}"
-				"gid=${toString config.users.groups.users.gid}"
-				# rwxr-xr-x
-				"file_mode=0755"
-				"dir_mode=0755"
-			];
-		};
-		in builtins.listToAttrs (
-		builtins.map (s: {
-			name = "${base}/${s.name}";
-			value = mkNasShare s.share;
-		}) shares
-		);
-	};
+		(lib.mkIf cfg.enable {
+			fileSystems = let
+			creds = config.age.secrets."nas/creds".path;
+			mkNasShare = share: {
+				device = address share;
+				fsType = "cifs";
+				options = [
+					"credentials=${creds}"
+					# Auto mount
+					"x-systemd.automount"
+					# unmount after 60s inactive
+					"x-systemd.idle-timeout=${toString unmount-after-idle-secs}"
+					# mount timeouts
+					"x-systemd.mount-timeout=${toString timeout}s"
+					"x-systemd.device-timeout=${toString timeout}s"
+					"_netdev" # mark as network dependent
+					"noauto" # don't mount at boot
+					"nofail" # don't fail hard
+					# so I own the files and dirs
+					"uid=${toString config.users.users.themarlstar.uid}"
+					"gid=${toString config.users.groups.users.gid}"
+					# rwxr-xr-x
+					"file_mode=0755"
+					"dir_mode=0755"
+				];
+			};
+			in builtins.listToAttrs (
+			builtins.map (s: {
+				name = "${cfg.mounts-base}/${s.name}";
+				value = mkNasShare s.share;
+			}) shares
+			);
+		})
+	];
 }
