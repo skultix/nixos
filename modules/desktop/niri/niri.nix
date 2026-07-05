a10f7949f211ff2c8c8a0b7d1871417bb0faed23
Marley Reeves <marley@marlstar.com>
2026-07-05 17:08:57 +1000

    fix(niri): make xwayland system

--- modules/desktop/niri/niri.nix
+++ modules/desktop/niri/niri.nix
@@ -1,4 +1,4 @@
-{ home, pkgs, ... }: {
+{ pkgs, ... }: {
 	programs.niri = {
 		enable = true;
 		package = pkgs.niri;
@@ -7,10 +7,7 @@
 	# niri-flake automaticlly creates a polkit daemon
 	# but I want to use my own configured polkit agent
 	systemd.user.services.niri-flake-polkit.enable = false;
-}
-// home {
 
-	home.packages = with pkgs; [
-		xwayland-satellite
-	];
+	# xwayland
+	environment.systemPackages = with pkgs; [ xwayland-satellite ];
 }
