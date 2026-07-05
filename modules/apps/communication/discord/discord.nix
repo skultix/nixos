adaded0464942f15a8860d4edf15a0afe7abfb0a
Marley Reeves <marley@marlstar.com>
2026-07-05 17:30:33 +1000

    discord nixcord

---
+++ modules/apps/communication/discord/discord.nix
@@ -0,0 +1,46 @@
+{ home, inputs, pkgs, ... }: home {
+	imports = [ inputs.nixcord.homeModules.nixcord ];
+
+	programs.nixcord = {
+		enable = true;
+
+		discord = {
+			equicord.enable = true;
+			openASAR.enable = true;
+			krisp.enable = true;
+			commandLineArgs = [
+				"--enable-features=VaapiVideoDecoder,MiddleClickAutoscroll"
+				"--ozone-platform-hint=auto"
+			];
+			settings = {
+				SKIP_HOST_UPDATE = true;
+			};
+		};
+
+		config = let
+		system24 = "https://raw.githubusercontent.com/refact0r/system24/refs/heads/main/theme/system24.theme.css";
+		in {
+			enabledThemeLinks = [ system24 ];
+
+			frameless = true;
+		};
+	};
+
+	stylix.targets.nixcord.enable = false;
+
+	xdg.autostart.entries = [
+		"${pkgs.discord}/share/applications/discord.desktop"
+	];
+
+	programs.niri.settings.window-rules = [
+		{ # Always open on comms workspace & maximised
+			matches = [ { app-id = "discord"; } ];
+			open-on-workspace = "communication";
+			open-maximized = true;
+		}
+		{ # Don't focus when autostarting at launch
+			matches = [ { app-id = "discord"; at-startup = true; } ];
+			open-focused = false;
+		}
+	];
+}
